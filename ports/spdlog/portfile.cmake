9c07a8a484f3153028ca8bfad9a4a943440a8cd6
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2020-02-21 11:36:08 +0100

    [fmt, spdlog] 6.1.2 1.5.0

--- ports/spdlog/portfile.cmake
+++ ports/spdlog/portfile.cmake
@@ -2,8 +2,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO gabime/spdlog
-    REF v1.4.2
-    SHA512 886b489138a1bdf363b5eab65a7d973f570a01c399ff5b910fcfee172ad4ff4d42a45dc2ae24e77b07130df527fb13a86b3e55ac992e72c418aebb232e27eabf
+    REF v1.5.0
+    SHA512 78991c943dd95af563c4b29545b9b5d635caf1af5031262dde734ecf70c0b4ae866d954ee77b050f9f0cc089a3bc57ee9583895e51cb00dd1cc6c10ff905ca34
     HEAD_REF master
 )
 
@@ -18,7 +18,7 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/${PORT}/cmake)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
 vcpkg_test_cmake(PACKAGE_NAME spdlog REQUIRED_HEADER spdlog/spdlog.h TARGETS spdlog::spdlog)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)

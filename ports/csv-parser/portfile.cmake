8c7184effc2aaa710f3816fe9a1d296d143e175c
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2020-03-31 15:35:05 +0200

    [csv-parser] 1.3.0

--- ports/csv-parser/portfile.cmake
+++ ports/csv-parser/portfile.cmake
@@ -2,9 +2,10 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO vincentlaucsb/csv-parser
-    REF 1.2.3
-    SHA512 a6e953ec811d0c0d407d29fc77ff86709fa6e67474639ef5581609a39b2d1582423a450a8ba86a861922965a0816a30ed85c8bae56661f38247b75e4a750514f
+    REF 1.3.0
+    SHA512 b2496bd6a9906ac46f652d252b6f5402291f6bec85cb11c6607c8c8f25244ceb99da503da579d6c8e5299724b7004da315b6f19a1e1bb849545abc01d92e08dd
     HEAD_REF master
+    PATCHES clang-win.patch
 )
 
 file(INSTALL ${SOURCE_PATH}/single_include/csv.hpp DESTINATION ${CURRENT_PACKAGES_DIR}/include)

afd0a768cb70fef3eb1b59384ad74c30c9b57d89
Dirk Vanden Boer <dirk.vdb@gmail.com>
2020-04-06 22:04:27 +0200

    [giflib] update port

--- ports/giflib/portfile.cmake
+++ ports/giflib/portfile.cmake
@@ -1,25 +1,27 @@
-set(MAJOR 5)
-set(MINOR 1)
-set(REVISION 4)
-set(VERSION ${MAJOR}.${MINOR}.${REVISION})
-set(PACKAGE_NAME ${PORT}-${MAJOR}.${MINOR}.${REVISION})
-set(PACKAGE ${PACKAGE_NAME}.tar.bz2)
-
 include(vcpkg_common_functions)
+
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+set(GIFLIB_VERSION 5.1.4)
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://downloads.sourceforge.net/sourceforge/${PORT}/${PACKAGE}"
-    FILENAME "${PACKAGE}"
+    URLS "https://nchc.dl.sourceforge.net/project/giflib/giflib-${GIFLIB_VERSION}.tar.bz2"
+    FILENAME "giflib-${GIFLIB_VERSION}.tar.bz2"
     SHA512 32b5e342056c210e6478e9cb3b6ceec9594dcfaf34feea1eb4dad633a081ed4465bceee578c19165907cb47cb83912ac359ceea666a8e07dbbb5420f9928f96d
 )
+
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
-    PATCHES msvc-guard-unistd-h.patch
+    REF ${GIFLIB_VERSION}
+    PATCHES
+        msvc-guard-unistd-h.patch
 )
+
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
     OPTIONS_DEBUG
         -DGIFLIB_SKIP_HEADERS=ON
 )

ab0f41b691a613e6c31baa642c3d3cce6981b7c4
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2021-04-16 14:18:13 +0200

    [spdlog] 1.8.5

--- ports/spdlog/portfile.cmake
+++ ports/spdlog/portfile.cmake
@@ -2,8 +2,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO gabime/spdlog
-    REF v1.8.2
-    SHA512 7a0a2353a10187cc314253b366fc46be8f9fe2480d2cbac3a96a8e6825ee4b62b0a5ebb3add2b22b5d7ca8fe6dddd963926603e5296e3431c0a4f7ac42beda7f
+    REF v1.8.5
+    SHA512 77cc9df0c40bbdbfe1f3e5818dccf121918bfceac28f2608f39e5bf944968b7e8e24a6fc29f01bc58a9bae41b8892d49cfb59c196935ec9868884320b50f130c
     HEAD_REF master
 )
 

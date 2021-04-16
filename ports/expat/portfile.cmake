e30ae83dfdde7ad10a1fa522583742ba72c6e583
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2021-04-16 14:18:24 +0200

    [expat] 2.3.0

--- ports/expat/portfile.cmake
+++ ports/expat/portfile.cmake
@@ -1,6 +1,6 @@
 set(MAJOR 2)
-set(MINOR 2)
-set(REVISION 7)
+set(MINOR 3)
+set(REVISION 0)
 set(VERSION R_${MAJOR}_${MINOR}_${REVISION})
 
 include(vcpkg_common_functions)
@@ -8,8 +8,9 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libexpat/libexpat
     REF ${VERSION}
-    SHA512 11b1f9a135c4501ef0112e17da8381e956366165a11a384daedd4cdef9a00c3112c8f6ff8c8f6d3b5e7b71b9a73041f23beac0f27e9cfafe1ec0266d95870408
+    SHA512 728ceb1d912fabc7662d4201828dc29c7fdaff124a1d6dc81434b1951eb530b17f1bbb1aec22c1520543cff3844f4751f29d3c59b696dd8a56598abdc25e1d08
     HEAD_REF master
+    PATCHES winpostfix.patch
 )
 
 if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL Emscripten)
@@ -28,10 +29,11 @@ vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}/expat
     PREFER_NINJA
     OPTIONS
-        -DBUILD_examples=OFF
-        -DBUILD_tests=OFF
-        -DBUILD_tools=OFF
-        -DBUILD_shared=${EXPAT_LINKAGE}
+        -DEXPAT_BUILD_EXAMPLES=OFF
+        -DEXPAT_BUILD_TESTS=OFF
+        -DEXPAT_BUILD_TOOLS=OFF
+        -DEXPAT_BUILD_DOCS=OFF
+        -DEXPAT_SHARED_LIBS=${EXPAT_LINKAGE}
 )
 
 vcpkg_install_cmake()
@@ -42,8 +44,15 @@ file(INSTALL ${SOURCE_PATH}/expat/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/sh
 vcpkg_fixup_pkgconfig_file()
 vcpkg_copy_pdbs()
 
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/cmake")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/cmake")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/doc")
+
 file(READ ${CURRENT_PACKAGES_DIR}/include/expat_external.h EXPAT_EXTERNAL_H)
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     string(REPLACE "!defined(XML_STATIC)" "/* vcpkg static build !defined(XML_STATIC) */ 0" EXPAT_EXTERNAL_H "${EXPAT_EXTERNAL_H}")
 endif()
 file(WRITE ${CURRENT_PACKAGES_DIR}/include/expat_external.h "${EXPAT_EXTERNAL_H}")
+
+vcpkg_test_cmake(PACKAGE_NAME EXPAT MODULE REQUIRED_HEADER expat.h TARGETS EXPAT::EXPAT)

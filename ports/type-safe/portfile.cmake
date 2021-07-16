747edc6dac574feecf48e5a8937324d5d4dda0b2
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2021-07-16 12:16:18 +0200

    [type-safe] update to latest git version

--- ports/type-safe/portfile.cmake
+++ ports/type-safe/portfile.cmake
@@ -1,11 +1,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO foonathan/type_safe
-    REF da1d15abc612afbdc81d70c817b49ba1752177de
-    SHA512 5b344af89378e34f05d96bff2de61615bc16e21601d9fe9d0886c71db211bd3b42afb2467dd2eb7f3d11176dc9adc2d71c6dc0b60722e12aaf8c1d79ea869289
+    REF b09a8d3588364efa48d7913974ebacb7d67dd73f
+    SHA512 84c45fb088eb384626431527d4201a52bcff0de18f19c8382377bfd5a99e6d1ea219244caec2e73b85fbdca4d2cf684d2949ed4219472ef11347ee210c4949ee
     HEAD_REF v0.2.1
-    PATCHES
-        disable_tests.patch
 )
 
 vcpkg_configure_cmake(

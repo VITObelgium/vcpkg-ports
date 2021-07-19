9d0e5c32499b93bbe761ee547b258efc371b2cff
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2021-07-19 14:00:28 +0200

    [qt6] buildable on windows

--- ports/qt6/portfile.cmake
+++ ports/qt6/portfile.cmake
@@ -1,4 +1,7 @@
-set(VERSION 6.1.1)
+set(MAJOR 6)
+set(MINOR 1)
+set(REVISION 1)
+set(VERSION ${MAJOR}.${MINOR}.${REVISION})
 set(PACKAGE qt-everywhere-src-${VERSION}.tar.xz)
 
 vcpkg_download_distfile(ARCHIVE
@@ -11,7 +14,10 @@ vcpkg_extract_source_archive_ex(
     ARCHIVE ${ARCHIVE}
     REF ${PORT}
     OUT_SOURCE_PATH SOURCE_PATH
-    PATCHES config_install.patch
+    PATCHES
+        config_install.patch
+        harfbuzz.patch
+        optional-printsupport.patch # included in next release
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -36,18 +42,10 @@ get_filename_component(PERL_DIR ${PERL} DIRECTORY)
 message(STATUS "Perl directory: ${PERL_DIR}")
 vcpkg_add_to_path(${PERL_DIR})
 
-if (VCPKG_TARGET_IS_LINUX)
-    set(FREETYPE_HARFBUZZ ON)
-else ()
-    set(FREETYPE_HARFBUZZ OFF)
-endif ()
+set(SECURETRANSPORT)
 
-if (VCPKG_TARGET_IS_WINDOWS)
-    set(DESKTOP_OPENGL ON)
-    set(DYNAMIC_OPENGL OFF)
-else ()
-    set(DESKTOP_OPENGL OFF)
-endif ()
+set (FREETYPE_HARFBUZZ system)
+set (OPENGL desktop)
 
 if (VCPKG_TARGET_IS_OSX)
     set(ALLOW_SYSTEM_LIBS ON)
@@ -55,13 +53,45 @@ else ()
     set(ALLOW_SYSTEM_LIBS OFF)
 endif()
 
+# ssl configuration
+set(SSL no)
+set(OPENSSL no)
+set(SECURETRANSPORT OFF)
+set(SCHANNEL OFF)
+if (FEATURE_ssl)
+    set(SSL yes)
+    if (VCPKG_TARGET_IS_LINUX)
+        set(OPENSSL yes)
+    elseif(VCPKG_TARGET_IS_OSX)
+        set(SECURETRANSPORT ON)
+    elseif(VCPKG_TARGET_IS_WINDOWS)
+        set(SCHANNEL ON)
+    endif()
+endif()
+
+# sql configuration
+set(SQLITE no)
+if (FEATURE_sql)
+    set(SQLITE system)
+endif()
+
+
 set(QT6_DIRECTORY_PREFIX "qt6/")
 set(qt_plugindir ${QT6_DIRECTORY_PREFIX}plugins)
 set(qt_qmldir ${QT6_DIRECTORY_PREFIX}qml)
 
+SET(QT_DECLARATIVE OFF)
+
+if (FEATURE_tools OR FEATURE_qml)
+    set(QT_DECLARATIVE ON)
+endif ()
+
+
 vcpkg_configure_cmake(
     SOURCE_PATH "${SOURCE_PATH}"
+    PREFER_NINJA
     OPTIONS
+        -DCMAKE_OBJECT_PATH_MAX=260
         -DINSTALL_BINDIR:STRING=tools/qt6
         -DINSTALL_LIBEXECDIR:STRING=bin
         -DINSTALL_PLUGINSDIR:STRING=${qt_plugindir}
@@ -81,25 +111,21 @@ vcpkg_configure_cmake(
         -DFEATURE_dbus=OFF
         -DFEATURE_zstd=OFF
         -DFEATURE_icu=OFF
-        -DFEATURE_pdf=OFF
         -DFEATURE_glib=OFF
         -DFEATURE_accessibility=OFF
         -DFEATURE_brotli=OFF
-        
-        -DINPUT_opengl=desktop
-        -DFEATURE_opengl=ON
-        -DFEATURE_opengl_desktop=ON
+        -DFEATURE_clang=OFF
+        -DFEATURE_clangcpp=OFF
+
+        -DINPUT_opengl=${OPENGL}
         
         -DFEATURE_system_zlib=ON
         -DFEATURE_system_png=ON
         -DFEATURE_system_pcre2=ON
         -DFEATURE_system_jpeg=ON
-        -DFEATURE_freetype=${FREETYPE_HARFBUZZ}
-        -DFEATURE_harfbuzz=${FREETYPE_HARFBUZZ}
-        -DFEATURE_system_freetype=${FREETYPE_HARFBUZZ}
-        -DFEATURE_system_harfbuzz=${FREETYPE_HARFBUZZ}
+
+        -DINPUT_freetype=${FREETYPE_HARFBUZZ}
         
-        -DBUILD_qtactiveqt=OFF
         -DBUILD_qtdoc=OFF
         -DBUILD_qt3d=OFF
         -DBUILD_qtquick3d=OFF
@@ -109,7 +135,6 @@ vcpkg_configure_cmake(
         -DBUILD_qtconnectivity=OFF
         -DBUILD_qtdatavis3d=OFF
         -DBUILD_qtcharts=${FEATURE_charts}
-        -DBUILD_qtdeclarative=OFF
         -DBUILD_qtmultimedia=OFF
         -DBUILD_qtpurchasing=OFF
         -DBUILD_qtremoteobjects=OFF
@@ -129,40 +154,57 @@ vcpkg_configure_cmake(
         -DBUILD_qtopcua=OFF
         ${OPTIONAL_CMAKE_ARGS}
 
-        -DBUILD_qttools=${FEATURE_tools}
-        -DBUILD_qttranslations=${FEATURE_tools}
-
         -DBUILD_qtquickcontrols=${FEATURE_qml}
         -DBUILD_qtquickcontrols2=${FEATURE_qml}
         -DBUILD_qtlottie=${FEATURE_qml}
         -DBUILD_qtquicktimeline=${FEATURE_qml}
-        -DBUILD_qtdeclarative=${FEATURE_qml}
         -DBUILD_qtwayland=${FEATURE_qml}
-
+        -DFEATURE_qml_devtools=OFF
+        
         -DBUILD_qtlocation=${FEATURE_location}
         -DFEATURE_geoservices_here=OFF
         -DFEATURE_geoservices_mapbox=OFF
         -DFEATURE_geoservices_mapboxgl=OFF
 
-        -DFEATURE_ssl=${FEATURE_ssl}
-        -DFEATURE_securetransport=${FEATURE_ssl}
-        -DFEATURE_=${FEATURE_ssl}
+        -DINPUT_ssl=${SSL}
+        -DINPUT_openssl=${OPENSSL}
+        -DFEATURE_securetransport=${SECURETRANSPORT}
+        -DFEATURE_schannel=${SCHANNEL}
 
         -DFEATURE_sql=${FEATURE_sql}
-        # -no-sql-db2
-        # -no-sql-ibase
-        # -no-sql-mysql
-        # -no-sql-oci
-        # -no-sql-odbc
-        # -no-sql-psql
-        # -no-sql-sqlite
+        -DINPUT_sqlite=${SQLITE}
+        -DFEATURE_sql_psql=OFF
+        -DFEATURE_sql_odbc=OFF
+        -DFEATURE_sql_db2=OFF
+        -DFEATURE_sql_ibase=OFF
+        -DFEATURE_sql_mysql=OFF
+        -DFEATURE_sql_oci=OFF
 
         -DCMAKE_FEATURE_TIFF=${FEATURE_tiff}
         -DFEATURE_system_tiff=ON
+
+        -DBUILD_qtdeclarative=${QT_DECLARATIVE}
+        -DBUILD_qtactiveqt=${FEATURE_tools}
+        -DBUILD_qttools=${FEATURE_tools}
+        -DBUILD_qttranslations=${FEATURE_tools}
+        -DFEATURE_pdf=${FEATURE_tools}
+        -DFEATURE_printsupport=${FEATURE_tools}
+        -DFEATURE_linguist=${FEATURE_tools}
+        -DFEATURE_qev=${FEATURE_tools}
+        -DFEATURE_designer=${FEATURE_tools} # cannot be disabled, otherwise qtlinguist fails as it needs UiTools
+        -DFEATURE_quick_designer=OFF
+        -DFEATURE_qdbus=OFF
+        -DFEATURE_kmap2qmap=OFF
+        -DFEATURE_distancefieldgenerator=OFF
+        -DFEATURE_pixeltool=OFF
+        -DFEATURE_assistant=OFF
+        
+    OPTIONS_RELEASE
+        -DINPUT_debug=no
+        -DFEATURE_optimize_full=ON
         
     OPTIONS_DEBUG
-        -DINPUT_debug:BOOL=ON
-        -DQT_NO_MAKE_TOOLS:BOOL=ON
+        -DINPUT_debug=ON
 )
 vcpkg_install_cmake()
 vcpkg_copy_pdbs()

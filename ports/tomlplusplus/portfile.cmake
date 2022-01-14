50139d5d6dfdbb76f0fa40727ccf6a503490f2ce
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2022-01-14 15:35:25 +0100

    [tomlplusplus] 3.0.0

--- ports/tomlplusplus/portfile.cmake
+++ ports/tomlplusplus/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO marzer/tomlplusplus
-    REF v2.4.0
-    SHA512 cfb8a1eeaed3350f8b5341b6893527c9571ee71416c0dc2d680d8739003cd5de85aad8efc0bdbf06e4ed7d3da0a942939509a86c035b551773df3e1b77afacbe
+    REF v3.0.0
+    SHA512 46a3d2b063af1ad4e414d61c19873c25ed222291a667b480f684cb43ccb9c67f522717da1530e6d6ca657bef2812135d2f1c4fb9b65f8b2a841fe0ddb66767ce
     HEAD_REF master
 )
 

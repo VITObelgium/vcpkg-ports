a9a55679866ea815e76b3e8c59ae854b60b96d6f
Dirk Vanden Boer <dirk.vdb@gmail.com>
2022-01-14 22:03:39 +0100

    [sqlpp11] 0.61

--- ports/sqlpp11/portfile.cmake
+++ ports/sqlpp11/portfile.cmake
@@ -1,23 +1,31 @@
 set(VERSION_MAJOR 0)
-set(VERSION_MINOR 59)
+set(VERSION_MINOR 61)
 set(VERSION ${VERSION_MAJOR}.${VERSION_MINOR})
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO rbock/sqlpp11
     REF ${VERSION}
-    SHA512 9da05e7a5163200040205b9740d6bf4ad1faa94b2bf031c16d896865b3f10e0fe95a0532a2c2e89adc051250a7f76c550a239916fdd700828d4fb1da566a4fe3
+    SHA512 f180f863a25f671d3909f807748568bde8d66c3c236bd5777780240b5cffe6c6545e9627762caf4a488b28939f459813a3298e7d6aa52e9443639639a55f67ab
     HEAD_REF master
     PATCHES
-        float-precision.patch
-        msvc-2019.patch
+        #float-precision.patch
+        #msvc-2019.patch
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        sqlite      BUILD_SQLITE3_CONNECTOR
+        postgresql  BUILD_POSTGRESQL_CONNECTOR
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
-        -DENABLE_TESTS=OFF
+        ${FEATURE_OPTIONS}
+        -DBUILD_TESTING=OFF
+        -DUSE_SYSTEM_DATE=ON
 )
 
 
@@ -40,5 +48,7 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
 
 vcpkg_copy_pdbs()
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/FindSqlpp11.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/cmake)
+#file(COPY ${CMAKE_CURRENT_LIST_DIR}/FindSqlpp11.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/cmake)
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+
+vcpkg_test_cmake(PACKAGE_NAME Sqlpp11 REQUIRED_HEADER sqlpp11/sqlpp11.h TARGETS sqlpp11::sqlpp11)

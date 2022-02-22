f975a78f8e17177fb2ce46c1a6591d7d61c905ef
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2022-02-22 16:33:21 +0100

    [type-safe] 0.2.2

--- ports/type-safe/portfile.cmake
+++ ports/type-safe/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_github(
     REPO foonathan/type_safe
     REF b09a8d3588364efa48d7913974ebacb7d67dd73f
     SHA512 84c45fb088eb384626431527d4201a52bcff0de18f19c8382377bfd5a99e6d1ea219244caec2e73b85fbdca4d2cf684d2949ed4219472ef11347ee210c4949ee
-    HEAD_REF v0.2.1
+    HEAD_REF v0.2.2
 )
 
 vcpkg_configure_cmake(

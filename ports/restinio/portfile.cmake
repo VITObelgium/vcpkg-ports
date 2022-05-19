6efa753b720a8dbfc97d56126068eec89ada5e26
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2022-05-19 14:35:26 +0200

    [restinio] 0.6.14

--- ports/restinio/portfile.cmake
+++ ports/restinio/portfile.cmake
@@ -1,54 +1,22 @@
-
-set(VERSION_MAJOR 0)
-set(VERSION_MINOR 6)
-set(VERSION_REVISION 0)
-set(VERSION ${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_REVISION})
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO Stiffstream/restinio
-    REF v.${VERSION}
-    SHA512 70c37a9b88db5eef0591baf0adc8227c1103a4f272c8fcd332410b9481854f5ae8f2385c8070c7eab17e8a75e699cded528fc655fbdef3553203914abb379b3a
-    HEAD_REF master
-    PATCHES recursivedeps.patch
+    REPO stiffstream/restinio
+    REF 20140bcecaec6f44ad5a8f68efcd8b44e1375604 # v.0.6.14
+    SHA512 e4654342831cb5c9086b60b22a7a15dd68a6769e28936576a1ff61352ea204f8e171bd446d002cefb514fd0cc4842878f23d5d51bc0da48c6224b96e4a0f3b14
+    PATCHES
+        fmt.patch
+        recursivedeps.patch
 )
 
-vcpkg_replace_string(${SOURCE_PATH}/dev/CMakeLists.txt "find_package(Catch2 CONFIG REQUIRED)" "")
-vcpkg_replace_string(${SOURCE_PATH}/dev/restinio/CMakeLists.txt "fmt::fmt-header-only" "fmt::fmt")
-
-if("boost" IN_LIST FEATURES)
-    set(ASIO_OPT -DRESTINIO_USE_BOOST_ASIO=static)
-elseif("asio" IN_LIST FEATURES)
-    set(ASIO_OPT -DRESTINIO_USE_BOOST_ASIO=none)
-else()
-    message(FATAL_ERROR "boost or asio must be active as a feature")
-endif()
-
-set (USE_STATIC_CRT_VALUE OFF)
-if(VCPKG_CRT_LINKAGE STREQUAL "static")
-    set(USE_STATIC_CRT_VALUE ON)
-endif()
-
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}/dev
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/vcpkg"
     OPTIONS
-        -DRESTINIO_INSTALL=ON
-        -DRESTINIO_TEST=OFF
-        -DRESTINIO_SAMPLE=OFF
-        -DRESTINIO_BENCH=OFF
-        -DRESTINIO_ALLOW_SOBJECTIZER=OFF
-        -DRESTINIO_FIND_DEPS=ON
-        -DBoost_USE_STATIC_RUNTIME=${USE_STATIC_CRT_VALUE}
-        ${ASIO_OPT}
+        -DRESTINIO_FMT_HEADER_ONLY=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/restinio)
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH "lib/cmake/restinio")
-
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib ${CURRENT_PACKAGES_DIR}/debug)
-
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib" "${CURRENT_PACKAGES_DIR}/debug")
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)

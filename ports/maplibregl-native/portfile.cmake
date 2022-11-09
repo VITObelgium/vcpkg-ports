5a45abeeece99d8dc3c756a4b8e1507de5ec637b
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2022-11-09 21:01:13 +0100

    [maplibregl] fixed patches

--- ports/maplibregl-native/portfile.cmake
+++ ports/maplibregl-native/portfile.cmake
@@ -53,7 +53,7 @@ vcpkg_apply_patches(
         cmake-fixes-qt.patch
         compression-zlib.patch
         cmake-install-vendor-headers.patch
-        #glyph-rasterizer.patch
+        glyph-rasterizer.patch
 )
     
 vcpkg_configure_cmake(
@@ -71,7 +71,7 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/QMapLibreGL)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/QMapLibreGL TARGET_PATH share/QMapLibreGL)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 

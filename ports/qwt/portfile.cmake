1e456b0255a93d818156f8b8856567b22988142f
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2023-03-14 08:39:37 +0100

    [qwt] 6.2.0 with qt6 support

--- ports/qwt/portfile.cmake
+++ ports/qwt/portfile.cmake
@@ -1,67 +1,32 @@
-if (VCPKG_TARGET_IS_OSX)
-    set(VCPKG_BUILD_TYPE release)
-endif ()
-
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://sourceforge.net/projects/qwt/files/qwt/6.1.4/qwt-6.1.4.zip"
-    FILENAME "qwt-6.1.4.zip"
-    SHA512 711256ec4d1d54d201650e9c63b584b17a61dbedde5a581e0b6227fdbee149cdbe2a889aa58f67777125b6471fc1fe248992ec845eb6481a2dfc2c4276701d7f
-)
-
-vcpkg_extract_source_archive_ex(
-    ARCHIVE ${ARCHIVE}
+vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
-    PATCHES fix-dynamic-static.patch
+    URL "http://git.code.sf.net/p/qwt/git"
+    REF "06d6822b595b70c9fd567a4fe0d835759bf271fe"
+    FETCH_REF qwt-6.2
+    PATCHES 
+        config.patch
+        fix_dll_install.patch
 )
 
-set(DEBUG_DIR "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg")
-set(RELEASE_DIR "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel")
-
-vcpkg_configure_qmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    OPTIONS
-        CONFIG+=${VCPKG_LIBRARY_LINKAGE}
+string(COMPARE EQUAL  "${VCPKG_LIBRARY_LINKAGE}" "dynamic" IS_DYNAMIC)
+set(OPTIONS "")
+if(IS_DYNAMIC)
+    set(OPTIONS "QWT_CONFIG+=QwtDll")
+endif()
+vcpkg_qmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    QMAKE_OPTIONS
+        ${OPTIONS}
+        "CONFIG-=debug_and_release"
+        "CONFIG+=create_prl"
+        "CONFIG+=link_prl"
 )
+vcpkg_qmake_install()
+vcpkg_copy_pdbs()
 
-if (CMAKE_HOST_WIN32)
-    vcpkg_build_qmake(
-        RELEASE_TARGETS sub-src-release_ordered
-        DEBUG_TARGETS sub-src-debug_ordered
-    )
-elseif (CMAKE_HOST_UNIX OR CMAKE_HOST_APPLE) # Build in UNIX
-    vcpkg_build_qmake(
-        RELEASE_TARGETS sub-src-all-ordered
-        DEBUG_TARGETS sub-src-all-ordered
-    )
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
-
-#Set the correct install directory to packages
-foreach(MAKEFILE ${RELEASE_MAKEFILES} ${DEBUG_MAKEFILES})
-    vcpkg_replace_string(${MAKEFILE} "(INSTALL_ROOT)${INSTALLED_DIR_WITHOUT_DRIVE}" "(INSTALL_ROOT)${PACKAGES_DIR_WITHOUT_DRIVE}")
-endforeach()
-
-#Install the header files
-file(GLOB HEADER_FILES ${SOURCE_PATH}/src/*.h)
-file(INSTALL ${HEADER_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/include/qwt)
-
-#Install the module files
-if (CMAKE_HOST_WIN32)
-    file(INSTALL ${RELEASE_DIR}/lib/qwt.lib DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-    file(INSTALL ${DEBUG_DIR}/lib/qwtd.lib DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
-
-    if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-        file(INSTALL ${RELEASE_DIR}/lib/qwt.dll DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-        file(INSTALL ${DEBUG_DIR}/lib/qwtd.dll DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-    endif()
-    vcpkg_copy_pdbs()
-elseif (CMAKE_HOST_UNIX OR CMAKE_HOST_APPLE) # Build in UNIX
-    file(INSTALL ${RELEASE_DIR}/lib/libqwt.a DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-    if (NOT VCPKG_TARGET_IS_OSX)
-        file(INSTALL ${DEBUG_DIR}/lib/libqwt.a DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
-    endif ()
-endif()
-
 
 # Handle copyright
-file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/qwt)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/qwt/COPYING ${CURRENT_PACKAGES_DIR}/share/qwt/copyright)
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)

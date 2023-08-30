d6ff226122d37d0d7f367b5eab495a70130ce1e2
Dirk Vanden Boer <dirk.vdb@gmail.com>
2023-08-30 11:53:39 +0200

    [netcdf-fortran] 4.6.1

--- ports/netcdf-fortran/portfile.cmake
+++ ports/netcdf-fortran/portfile.cmake
@@ -1,28 +1,15 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Unidata/netcdf-fortran
-    REF v4.6.0
-    SHA512 2488c0d9d9df49c56253dda1a411c50bb32a52651bd5d43e303190ca7de06db3f48621da0c7faf48aaee01f7d74031b199fcfbecb2ba523a994c00470680f014
+    REF v4.6.1
+    SHA512 3cf3616610bcb7b06c7a78eb47ccf4decc695360d96e8514939a53a48f49f2a659ee77de9113673fc95fdc82581a264d781a6c18ef7127d8c13196c49e825a34
     HEAD_REF master
 )
-
-set(CONDITIONAL_LIBS "")
-if (EXISTS ${CURRENT_INSTALLED_DIR}/share/hdf5)
-    list(APPEND CONDITIONAL_LIBS -lhdf5_hl -lhdf5)
-else ()
-    message(FATAL_ERROR "${CURRENT_INSTALLED_DIR}/share/hdf5 does not exist")
-endif ()
-
-if (EXISTS ${CURRENT_INSTALLED_DIR}/share/openmpi)
-    list(APPEND CONDITIONAL_LIBS -lmpi)
-endif ()
-configure_file(${CMAKE_CURRENT_LIST_DIR}/cacheinit.cmake.in ${SOURCE_PATH}/cacheinit.cmake)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
-        -C${SOURCE_PATH}/cacheinit.cmake
         -DCMAKE_REQUIRED_INCLUDES=${CURRENT_INSTALLED_DIR}/include
         -DCMAKE_REQUIRED_LINK_OPTIONS=-L${CURRENT_INSTALLED_DIR}/lib
         -DBUILD_SHARED_LIBS=OFF

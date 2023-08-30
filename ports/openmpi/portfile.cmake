16893bfa66536cff4111fe8a035c8805c20bf74a
Dirk Vanden Boer <dirk.vdb@gmail.com>
2023-08-30 11:52:27 +0200

    [mpi] [openmpi] mpi support

--- ports/openmpi/portfile.cmake
+++ ports/openmpi/portfile.cmake
@@ -1,23 +1,24 @@
-if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore" OR NOT VCPKG_CMAKE_SYSTEM_NAME)
-    message(FATAL_ERROR "This port is only for openmpi on Unix-like systems")
-endif()
-
 vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
-set(OpenMPI_FULL_VERSION "4.0.3")
-set(OpenMPI_SHORT_VERSION "4.0")
-set(VCPKG_BUILD_TYPE "release")
+set(OpenMPI_FULL_VERSION "4.1.3")
+set(OpenMPI_SHORT_VERSION "4.1")
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://download.open-mpi.org/release/open-mpi/v${OpenMPI_SHORT_VERSION}/openmpi-${OpenMPI_FULL_VERSION}.tar.gz"
-    FILENAME "openmpi-${OpenMPI_FULL_VERSION}.tar.gz"
-    SHA512 23a9dfb7f4a63589b82f4e073a825550d3bc7e6b34770898325323ef4a28ed90b47576acaae6be427eb2007b37a88e18c1ea44d929b8ca083fe576ef1111fef6
+  URLS "https://download.open-mpi.org/release/open-mpi/v${OpenMPI_SHORT_VERSION}/openmpi-${OpenMPI_FULL_VERSION}.tar.gz"
+  FILENAME "openmpi-${OpenMPI_FULL_VERSION}.tar.gz"
+  SHA512 f7b177121863ef79df6106639d18a89c028442b1314340638273b12025c4dc2cf9b5316cb7e6ecca8b65a51ee40a306a6b0970d7cce727fbb269a14f89af3161
 )
 
-vcpkg_extract_source_archive_ex(
+vcpkg_extract_source_archive(
+    SOURCE_PATH
     ARCHIVE ${ARCHIVE}
-    OUT_SOURCE_PATH SOURCE_PATH
+    PATCHES
+        keep_isystem.patch
 )
+
+vcpkg_find_acquire_program(PERL)
+get_filename_component(PERL_PATH ${PERL} DIRECTORY)
+vcpkg_add_to_path(${PERL_PATH})
 
 TEST_FEATURE("fortran" ENABLE_FORTRAN)
 if (ENABLE_FORTRAN)
@@ -26,25 +27,21 @@ else ()
     set(CONFIG_OPTS --disable-mpi-fortran)
 endif ()
 
-vcpkg_configure_autoconf(
-    SOURCE_PATH ${SOURCE_PATH}
-    OPTIONS
-        --bindir=${CURRENT_PACKAGES_DIR}/tools
-        --disable-java
-        --disable-visibility # handled by the compiler flags
-        ${CONFIG_OPTS}
-    OPTIONS_DEBUG
-        --enable-debug
+vcpkg_configure_make(
+        COPY_SOURCE
+        SOURCE_PATH ${SOURCE_PATH}
+        OPTIONS
+            --with-hwloc=internal
+            --with-libevent=internal
+            ${CONFIG_OPTS}
+        OPTIONS_DEBUG
+            --enable-debug
 )
 
-# make sure the prefix paths in the binary point to the installed dir and not the packages dir
-vcpkg_replace_string(
-    "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/opal/mca/installdirs/config/install_dirs.h"
-    "${CURRENT_PACKAGES_DIR}"
-    "${CURRENT_INSTALLED_DIR}"
-)
+vcpkg_install_make()
+vcpkg_fixup_pkgconfig()
 
-vcpkg_install_autotools()
-vcpkg_fixup_pkgconfig_mod(NAMES ompi-c ompi-cxx ompi-f90 ompi-fort ompi orte)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/openmpi RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)

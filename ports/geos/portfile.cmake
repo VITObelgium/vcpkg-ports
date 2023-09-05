16fec01ee7d2010678e55f2ec0a87b026dec0d73
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2023-09-05 10:21:19 +0200

    [geos] 3.12.0

--- ports/geos/portfile.cmake
+++ ports/geos/portfile.cmake
@@ -1,6 +1,6 @@
 set(VERSION_MAJOR 3)
-set(VERSION_MINOR 10)
-set(VERSION_REVISION 2)
+set(VERSION_MINOR 12)
+set(VERSION_REVISION 0)
 set(VERSION ${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_REVISION})
 set(PACKAGE_NAME ${PORT}-${VERSION})
 set(PACKAGE ${PACKAGE_NAME}.tar.bz2)
@@ -9,14 +9,13 @@ set(PACKAGE ${PACKAGE_NAME}.tar.bz2)
 vcpkg_download_distfile(ARCHIVE
     URLS "http://download.osgeo.org/geos/${PACKAGE}"
     FILENAME "${PACKAGE}"
-    SHA512 390381711ccf56b862c2736cf6329200822f121de1c49df52b8b85cabea8c7787b199df2196acacc2e5c677ff3ebe042d93d70e89deadbc19d754499edb65126
+    SHA512 cc67d9516d24ada2ceaf7c81ff9df40033bb15123160bf7e70fdff97b72c798f4b9ba8223c1eaebac0f33dcb530bc8cf42af000af3491f3227420290d89332d5
 )
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
     PATCHES
         charset.patch
-        fix-exported-includes.patch
 )
 
 vcpkg_configure_cmake(

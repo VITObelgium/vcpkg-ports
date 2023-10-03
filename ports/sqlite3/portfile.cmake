dab41a548cc8c45d83d6ea6bbcacf45aaac2750b
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2023-10-03 11:36:29 +0200

    [sqlite3] 3.43.1

--- ports/sqlite3/portfile.cmake
+++ ports/sqlite3/portfile.cmake
@@ -1,18 +1,22 @@
 
-set(VERSION 3.42.0)
-set(SQLITE_VERSION 3420000)
-set(SQLITE_HASH 76416dc40dd15611f59d7209087a08c6cf1b7584dee34da4ce11b75acd7f608d794747a7ed87c3ffe16b237699279c2da8f85b009a379e108d39f33bd727e1e1)
+set(VERSION 3.43.1)
+set(SQLITE_VERSION 3430100)
+set(SQLITE_HASH f17810f3b68b7f77a174503a863385a17bac0e9c819b9813cb75597cbd229ae8ad0b545410fc320669f377e79ab8412bbff8863f197d0f804c3a05b573df57e9)
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/sqlite-amalgamation-${SQLITE_VERSION})
 
 vcpkg_download_distfile(ARCHIVE
     URLS "https://sqlite.org/2023/sqlite-amalgamation-${SQLITE_VERSION}.zip"
     FILENAME "sqlite-amalgamation-${SQLITE_VERSION}.zip"
     SHA512 ${SQLITE_HASH})
-vcpkg_extract_source_archive(${ARCHIVE})
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE ${ARCHIVE}
+)
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
 set(SQLITE3_SKIP_TOOLS ON)
+
 if("tool" IN_LIST FEATURES)
     set(SQLITE3_SKIP_TOOLS OFF)
 endif()
@@ -21,9 +25,9 @@ vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
-        -DSQLITE3_SKIP_TOOLS=${SQLITE3_SKIP_TOOLS}
+    -DSQLITE3_SKIP_TOOLS=${SQLITE3_SKIP_TOOLS}
     OPTIONS_DEBUG
-        -DSQLITE3_SKIP_TOOLS=ON
+    -DSQLITE3_SKIP_TOOLS=ON
 )
 
 vcpkg_install_cmake()

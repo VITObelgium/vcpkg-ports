3a659d9f76a9741d3bc478f6095dc51681757d80
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2023-10-03 12:53:15 +0200

    [trompeloeil] v45

--- ports/trompeloeil/portfile.cmake
+++ ports/trompeloeil/portfile.cmake
@@ -1,27 +1,25 @@
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO rollbear/trompeloeil
-    REF v42
-    SHA512 50f9b517ba08331fe852ca27bc85b370930b3bfbe0e94523953f501d72fd5e53fa2ae12b9176782e49347e04eba038dd59a7013cad88493818330fc16175db25
+    REF v${VERSION}
+    SHA512 c82a0ff3057056a79890b5880c1755900370a4406dc7e231f9b81545014dfd85a3cea90f90f01931083ef7528360ec7f5f14a4d6b2c5578c59e89b6d28edf110 
     HEAD_REF master
 )
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-    OPTIONS
-        -DBUILD_TESTING=OFF
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/trompeloeil)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/trompeloeil)
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug ${CURRENT_PACKAGES_DIR}/lib)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
 
-if(NOT EXISTS ${CURRENT_PACKAGES_DIR}/include/trompeloeil.hpp)
+if(NOT EXISTS "${CURRENT_PACKAGES_DIR}/include/trompeloeil.hpp")
     message(FATAL_ERROR "Main includes have moved. Please update the forwarder.")
 endif()
 
-configure_file(${SOURCE_PATH}/LICENSE_1_0.txt ${CURRENT_PACKAGES_DIR}/share/trompeloeil/copyright COPYONLY)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE_1_0.txt")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/doc")

f4f11c15cc9934ff2fb5ddc2c69fed55748b43ef
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2023-11-29 21:00:42 +0100

    [benchmark] 1.8.3

--- ports/benchmark/portfile.cmake
+++ ports/benchmark/portfile.cmake
@@ -1,49 +1,30 @@
-if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
-    message(FATAL_ERROR "${PORT} does not currently support UWP")
-endif()
-
-if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-    message(STATUS "Warning: Dynamic building not supported yet. Building static.")
-    set(VCPKG_LIBRARY_LINKAGE static)
-endif()
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/benchmark
-    REF v1.5.5
-    SHA512 91a2cdf0245625c657b9bb8d76c7d616a99eaed333e42b34ceff4e996b8fa6027b1c7313a815e7b441d2f6e2e1244c139a0911d585bf0d2e767ac3cea23a67d1
-    HEAD_REF master
+    REF "v${VERSION}"
+    SHA512 4e12114251c79a426873cfba6e27270b69fc980cef9a68e9cb3170f8e2e203f77dee19ab1e65cad51cd67e60991d3bbfdd52553f22522ce5e6c611b5aa07602c
+    HEAD_REF main
+    PATCHES nopedantic.patch
 )
 
-# case fixes for mingw builds
-vcpkg_replace_string(${SOURCE_PATH}/src/colorprint.cc "<Windows.h>" "<windows.h>")
-vcpkg_replace_string(${SOURCE_PATH}/src/sleep.cc "<Windows.h>" "<windows.h>")
-vcpkg_replace_string(${SOURCE_PATH}/src/sleep.cc "<Shlwapi.h>" "<shlwapi.h>")
-vcpkg_replace_string(${SOURCE_PATH}/src/sysinfo.cc "<Windows.h>" "<windows.h>")
-vcpkg_replace_string(${SOURCE_PATH}/src/sysinfo.cc "<Shlwapi.h>" "<shlwapi.h>")
-vcpkg_replace_string(${SOURCE_PATH}/src/sysinfo.cc "<VersionHelpers.h>" "<versionhelpers.h>")
-vcpkg_replace_string(${SOURCE_PATH}/src/timers.cc "<Windows.h>" "<windows.h>")
-vcpkg_replace_string(${SOURCE_PATH}/src/timers.cc "<Shlwapi.h>" "<shlwapi.h>")
-vcpkg_replace_string(${SOURCE_PATH}/src/timers.cc "<VersionHelpers.h>" "<versionhelpers.h>")
-
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
     OPTIONS
         -DBENCHMARK_ENABLE_TESTING=OFF
+        -DBENCHMARK_ENABLE_WERROR=OFF
 )
 
-vcpkg_install_cmake()
-
+vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/benchmark)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/benchmark)
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 # Handle copyright
-file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/benchmark)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/benchmark/LICENSE ${CURRENT_PACKAGES_DIR}/share/benchmark/copyright)
-
-vcpkg_test_cmake(PACKAGE_NAME benchmark)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)

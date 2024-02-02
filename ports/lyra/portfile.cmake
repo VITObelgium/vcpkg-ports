a673ab273c14ff07b7a6d9e293fe47cf29ba6172
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2024-02-02 11:49:38 +0100

    [lyra] Support customizing help column width

--- ports/lyra/portfile.cmake
+++ ports/lyra/portfile.cmake
@@ -4,6 +4,7 @@ vcpkg_from_github(
     REF 1.6.1
     SHA512 643c25fbe996af2e888eacb99a715e3d420dbfc21d48756703cf301ab6ba0d1f8eea1cd0764bd5c173d2ddcef7c799448d8c3a77676024205163305e1363d461
     HEAD_REF master
+    PATCHES help-size.patch
 )
 
 vcpkg_configure_cmake(

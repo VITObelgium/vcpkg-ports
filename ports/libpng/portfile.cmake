677c68397c1a5ab3c1e295e3820f839dce792149
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2024-04-16 11:43:14 +0200

    [libpng] 1.7.1

--- ports/libpng/portfile.cmake
+++ ports/libpng/portfile.cmake
@@ -1,69 +1,106 @@
+# Download the apng patch
+set(LIBPNG_APNG_PATCH_PATH "")
+set(LIBPNG_APNG_OPTION "")
+if ("apng" IN_LIST FEATURES)
+    if(VCPKG_HOST_IS_WINDOWS)
+        # Get (g)awk and gzip installed
+        vcpkg_acquire_msys(MSYS_ROOT PACKAGES gawk gzip)
+        set(AWK_EXE_PATH "${MSYS_ROOT}/usr/bin")
+        vcpkg_add_to_path("${AWK_EXE_PATH}")
+    endif()
+    
+    set(LIBPNG_APNG_PATCH_NAME "libpng-${VERSION}-apng.patch")
+    vcpkg_download_distfile(LIBPNG_APNG_PATCH_ARCHIVE
+        URLS "https://downloads.sourceforge.net/project/libpng-apng/libpng16/${VERSION}/${LIBPNG_APNG_PATCH_NAME}.gz"
+        FILENAME "${LIBPNG_APNG_PATCH_NAME}.gz"
+        SHA512 a724f7de486920cb119818f7172fb589bc2c3c1cc1f81bb5c4da0609ab108ef9ef7406cf689a20bc4e8da69647847f550ed497b3fa99a10539e9a0abf492c053
+    )
+    set(LIBPNG_APNG_PATCH_PATH "${CURRENT_BUILDTREES_DIR}/src/${LIBPNG_APNG_PATCH_NAME}")
+    if (NOT EXISTS "${LIBPNG_APNG_PATCH_PATH}")
+        file(INSTALL "${LIBPNG_APNG_PATCH_ARCHIVE}" DESTINATION "${CURRENT_BUILDTREES_DIR}/src")
+        vcpkg_execute_required_process(
+            COMMAND gzip -d "${LIBPNG_APNG_PATCH_NAME}.gz"
+            WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}/src"
+            ALLOW_IN_DOWNLOAD_MODE
+            LOGNAME extract-patch.log
+        )
+    endif()
+    set(LIBPNG_APNG_OPTION "-DPNG_PREFIX=a")
+endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO glennrp/libpng
-    REF v1.6.37
-    SHA512 ccb3705c23b2724e86d072e2ac8cfc380f41fadfd6977a248d588a8ad57b6abe0e4155e525243011f245e98d9b7afbe2e8cc7fd4ff7d82fcefb40c0f48f88918
+    REF v${VERSION}
+    SHA512 3bb2a7b73113be42b09c2116e6c6f5a7ddb4e2ab06e0b13e10b7314acdccc4bb624ff602e16140c0484f6cde80efa190296226be3da195c6926819f07c723c12
     HEAD_REF master
-    PATCHES macos-arch-fix.patch
+    PATCHES
+        "${LIBPNG_APNG_PATCH_PATH}"
+        cmake.patch
+        fix-export-targets.patch
+        libm.patch
+        pkgconfig.patch
+        fix-msa-support-for-mips.patch
 )
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-    set(PNG_STATIC_LIBS OFF)
-    set(PNG_SHARED_LIBS ON)
-else()
-    set(PNG_STATIC_LIBS ON)
-    set(PNG_SHARED_LIBS OFF)
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" PNG_SHARED)
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" PNG_STATIC)
+
+vcpkg_list(SET LIBPNG_HARDWARE_OPTIMIZATIONS_OPTION)
+if(VCPKG_TARGET_IS_IOS)
+    vcpkg_list(APPEND LIBPNG_HARDWARE_OPTIMIZATIONS_OPTION "-DPNG_HARDWARE_OPTIMIZATIONS=OFF")
 endif()
 
-if (VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux" OR VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Darwin" OR VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Emscripten")
-    # finding the math library fails on some toolchains
-    # on apple libm is not presen on disk, so detection fails
-    set(EXTRA_OPTIONS -DM_LIBRARY=m)
-endif ()
+vcpkg_list(SET LD_VERSION_SCRIPT_OPTION)
+if(VCPKG_TARGET_IS_ANDROID)
+    vcpkg_list(APPEND LD_VERSION_SCRIPT_OPTION "-Dld-version-script=OFF")
+    if(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
+        vcpkg_cmake_get_vars(cmake_vars_file)
+        include("${cmake_vars_file}")
+        if(VCPKG_DETECTED_CMAKE_ANDROID_ARM_NEON)
+            vcpkg_list(APPEND LIBPNG_HARDWARE_OPTIMIZATIONS_OPTION "-DPNG_ARM_NEON=on")
+        else()
+            # for armeabi-v7a, check whether NEON is available
+            vcpkg_list(APPEND LIBPNG_HARDWARE_OPTIMIZATIONS_OPTION "-DPNG_ARM_NEON=check")
+        endif()
+    endif()
+endif()
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DPNG_STATIC=${PNG_STATIC_LIBS}
-        -DPNG_SHARED=${PNG_SHARED_LIBS}
+        ${LIBPNG_APNG_OPTION}
+        ${LIBPNG_HARDWARE_OPTIMIZATIONS_OPTION}
+        ${LD_VERSION_SCRIPT_OPTION}
+        -DPNG_STATIC=${PNG_STATIC}
+        -DPNG_SHARED=${PNG_SHARED}
         -DPNG_TESTS=OFF
         -DSKIP_INSTALL_PROGRAMS=ON
         -DSKIP_INSTALL_EXECUTABLES=ON
-        -DSKIP_INSTALL_FILES=ON
-        -DSKIP_INSTALL_SYMLINK=ON
-        -DPNG_ARM_NEON=on
-        ${EXTRA_OPTIONS}
+        -DSKIP_INSTALL_FILES=OFF
     OPTIONS_DEBUG
         -DSKIP_INSTALL_HEADERS=ON
+    MAYBE_UNUSED_VARIABLES
+        PNG_ARM_NEON
 )
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/libpng)
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/png")
 
-vcpkg_install_cmake()
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    if(EXISTS ${CURRENT_PACKAGES_DIR}/lib/libpng16_static.lib)
-        file(RENAME ${CURRENT_PACKAGES_DIR}/lib/libpng16_static.lib ${CURRENT_PACKAGES_DIR}/lib/libpng.lib)
+vcpkg_fixup_pkgconfig()
+if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
+    if(NOT VCPKG_BUILD_TYPE)
+        vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/libpng16.pc" "-lpng16" "-llibpng16d")
+        file(INSTALL "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/libpng16.pc" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig" RENAME "libpng.pc")
     endif()
-    if(EXISTS ${CURRENT_PACKAGES_DIR}/debug/lib/libpng16_staticd.lib)
-        file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/libpng16_staticd.lib ${CURRENT_PACKAGES_DIR}/debug/lib/libpngd.lib)
-    endif()
-
-    if(EXISTS ${CURRENT_PACKAGES_DIR}/lib/libpng16.a)
-        file(RENAME ${CURRENT_PACKAGES_DIR}/lib/libpng16.a ${CURRENT_PACKAGES_DIR}/lib/libpng.a)
-    endif()
-    if(EXISTS ${CURRENT_PACKAGES_DIR}/debug/lib/libpng16d.a)
-        file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/libpng16d.a ${CURRENT_PACKAGES_DIR}/debug/lib/libpngd.a)
-    endif()
-
-    file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/lib/libpng.a)
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/lib/pkgconfig/libpng16.pc" "-lpng16" "-llibpng16")
+elseif(NOT VCPKG_BUILD_TYPE)
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/libpng16.pc" "-lpng16" "-lpng16d")
+    file(INSTALL "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/libpng16.pc" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig" RENAME "libpng.pc")
 endif()
-
-# Remove CMake config files as they are incorrectly generated and everyone uses built-in FindPNG anyway.
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/libpng ${CURRENT_PACKAGES_DIR}/debug/lib/libpng)
-file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/libpng)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/libpng/LICENSE ${CURRENT_PACKAGES_DIR}/share/libpng/copyright)
+file(INSTALL "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/libpng16.pc" DESTINATION "${CURRENT_PACKAGES_DIR}/lib/pkgconfig" RENAME "libpng.pc")
 
 vcpkg_copy_pdbs()
-
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")

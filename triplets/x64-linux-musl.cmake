df347133e7ab3b8f77b73b29ccd02e60ed7db37d
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2024-06-13 10:01:15 +0200

    Fix musl triplet

--- triplets/x64-linux-musl.cmake
+++ triplets/x64-linux-musl.cmake
@@ -5,4 +5,4 @@ set(VCPKG_CMAKE_SYSTEM_NAME Linux CACHE STRING "")
 set(VCPKG_SYSROOT /tools/toolchains/x86_64-multilib-linux-musl/x86_64-multilib-linux-musl/sysroot CACHE FILEPATH "")
 
 set(VCPKG_LINKER_FLAGS "-fuse-ld=gold" CACHE STRING "" FORCE)
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/toolchain-linux-musl.cmake" CACHE FILEPATH "" CACHE FILEPATH "")
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/toolchain-linux-musl.cmake" CACHE FILEPATH "")

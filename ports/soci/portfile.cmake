15e75b712390b0690807d1d1c8a4052b97f51fee
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2025-04-24 14:51:28 +0200

    [soci] 4.0.3

--- ports/soci/portfile.cmake
+++ ports/soci/portfile.cmake
@@ -1,58 +1,74 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO SOCI/soci
-    REF v4.0.3
+    REF "v${VERSION}"
     SHA512 d501f55e7e7408e46b4823fd8a97d6ef587f5db0f5b98434be8dfc5693c91b8c3b84a24454279c83142ab1cd1fa139c6e54d6d9a67397b2ead61650fcc88bcdb
     HEAD_REF master
+    PATCHES
+        dependencies.diff
+        usage-requirements.diff
+)
+file(REMOVE
+    "${SOURCE_PATH}/cmake/modules/FindPostgreSQL.cmake"
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" SOCI_DYNAMIC)
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" SOCI_STATIC)
 
-TEST_FEATURE("sqlite3" WITH_SQLITE3)
-TEST_FEATURE("postgresql" WITH_POSTGRESQL)
-
-# Handle features
-set(_COMPONENT_FLAGS "")
-foreach(_feature IN LISTS ALL_FEATURES)
-    # Uppercase the feature name and replace "-" with "_"
-    string(TOUPPER "${_feature}" _FEATURE)
-    string(REPLACE "-" "_" _FEATURE "${_FEATURE}")
-
-    # Turn "-DWITH_*=" ON or OFF depending on whether the feature
-    # is in the list.
-    if(_feature IN_LIST FEATURES)
-        list(APPEND _COMPONENT_FLAGS "-DWITH_${_FEATURE}=ON")
-    else()
-        list(APPEND _COMPONENT_FLAGS "-DWITH_${_FEATURE}=OFF")
-    endif()
-endforeach()
-
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-    OPTIONS
-        -DSOCI_TESTS=OFF
-        -DSOCI_CXX_C11=ON
-        -DSOCI_LIBDIR:STRING=lib # This is to always have output in the lib folder and not lib64 for 64-bit builds
-        -DLIBDIR:STRING=lib
-        -DSOCI_STATIC=${SOCI_STATIC}
-        -DSOCI_SHARED=${SOCI_DYNAMIC}
-        ${_COMPONENT_FLAGS}
-
-        -DWITH_MYSQL=OFF
-        -DWITH_ORACLE=OFF
-        -DWITH_FIREBIRD=OFF
-        -DWITH_DB2=OFF
+vcpkg_check_features(OUT_FEATURE_OPTIONS options
+    FEATURES
+        boost       WITH_BOOST
+        boost       CMAKE_REQUIRE_FIND_PACKAGE_Boost
+        empty       SOCI_EMPTY
+        mysql       WITH_MYSQL
+        odbc        WITH_ODBC
+        odbc        CMAKE_REQUIRE_FIND_PACKAGE_ODBC
+        postgresql  WITH_POSTGRESQL
+        sqlite3     WITH_SQLITE3
+    INVERTED_FEATURES
+        core        WITH_DB2
+        core        WITH_FIREBIRD
+        core        WITH_ORACLE
+        core        WITH_VALGRIND
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DSOCI_CXX11=ON
+        -DSOCI_SHARED=${SOCI_DYNAMIC}
+        -DSOCI_STATIC=${SOCI_STATIC}
+        -DSOCI_TESTS=OFF
+        ${options}
+    MAYBE_UNUSED_VARIABLES
+        CMAKE_REQUIRE_FIND_PACKAGE_Boost
+        CMAKE_REQUIRE_FIND_PACKAGE_ODBC
+)
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/cmake ${CURRENT_PACKAGES_DIR}/debug/cmake ${CURRENT_PACKAGES_DIR}/debug/include ${CURRENT_PACKAGES_DIR}/debug/share)
-
-# Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE_1_0.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-
-configure_file(${CMAKE_CURRENT_LIST_DIR}/FindSoci.cmake.in ${CURRENT_PACKAGES_DIR}/share/cmake/FindSoci.cmake @ONLY)
-
+vcpkg_cmake_install()
 vcpkg_copy_pdbs()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/SOCI)
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/soci/soci-platform.h" "ifdef SOCI_DLL" "if 1")
+endif()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(READ "${CURRENT_PORT_DIR}/usage" usage)
+set(backends ${FEATURES})
+list(REMOVE_ITEM backends core boost)
+if(backends STREQUAL "")
+    string(APPEND usage "
+This soci build doesn't include any backend and may not be useful.
+")
+endif()
+foreach(backend IN LISTS backends)
+    string(APPEND usage "
+    # Using the ${backend} backend directly
+    target_link_libraries(main PRIVATE $<IF:$<TARGET_EXISTS:SOCI::soci_${backend}>,SOCI::soci_${backend},SOCI::soci_${backend}_static>)
+")
+endforeach()
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" "${usage}")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE_1_0.txt")

ddf30ed6cb9291c6b36ffc6a70f45102156acc1e
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2025-05-21 19:27:26 +0200

    More static linker flags added for fortran project

--- triplets/x64-linux-cluster.cmake
+++ triplets/x64-linux-cluster.cmake
@@ -4,7 +4,7 @@ set(VCPKG_LIBRARY_LINKAGE static CACHE STRING "")
 set(VCPKG_CMAKE_SYSTEM_NAME Linux CACHE STRING "")
 
 set(LINKER_EXECUTABLE ld.gold CACHE FILEPATH "")
-set(VCPKG_LINKER_FLAGS "-static-libstdc++ -static-libgcc -fuse-ld=gold" CACHE STRING "" FORCE)
+set(VCPKG_LINKER_FLAGS "-static-libstdc++ -static-libgcc -static-libquadmath -static-libgfortran -fuse-ld=gold" CACHE STRING "" FORCE)
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/toolchain-linux-cluster.cmake" CACHE FILEPATH "")
 
 # make sure the static openmp library is used

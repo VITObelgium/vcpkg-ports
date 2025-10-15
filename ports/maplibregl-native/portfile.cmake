5a2f7f298a3db53456e3cb79589ebbc63ae53f79
Dirk Vanden Boer <dirk.vdb@gmail.com>
2025-10-15 19:38:55 +0200

    [maplibregl-native] buildable on linux

--- ports/maplibregl-native/portfile.cmake
+++ ports/maplibregl-native/portfile.cmake
@@ -1,48 +1,45 @@
-if (WIN32)
+if(VCPKG_HOST_IS_WIN32)
     vcpkg_add_to_path($ENV{ProgramFiles}/git/bin)
     vcpkg_add_to_path($ENV{ProgramW6432}/git/bin)
-endif ()
-find_package(Git)
-if (NOT Git_FOUND)
-    message(FATAL_ERROR "Could not find git binary")
-endif ()
+endif()
+
+vcpkg_find_acquire_program(GIT)
 
 file(REMOVE_RECURSE ${CURRENT_BUILDTREES_DIR}/src)
 
-vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-FEATURES
-    "qt5"           FEATURE_qt5
-    "qt6"           FEATURE_qt6
+set(GIT_URL "https://github.com/maplibre/maplibre-native.git")
+set(GIT_REV "android-v10.1.0")
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/${PORT})
+
+if(EXISTS ${SOURCE_PATH})
+    message(STATUS "Removing existing source directory")
+    file(REMOVE_RECURSE ${SOURCE_PATH})
+endif()
+
+make_directory(${SOURCE_PATH})
+
+message(STATUS "Cloning and fetching submodules")
+vcpkg_execute_required_process(
+    COMMAND ${GIT} clone --config core.longpaths=true --recurse-submodules ${GIT_URL} ${SOURCE_PATH}
+    WORKING_DIRECTORY ${SOURCE_PATH}
+    LOGNAME clone
 )
 
-if (FEATURE_qt5 EQUAL FEATURE_qt6)
-    message(FATAL_ERROR "Enable either qt5 or qt6 support")
-endif ()
-
-vcpkg_from_git_mod(
-    URL https://github.com/maplibre/maplibre-native.git
-    OUT_SOURCE_PATH SOURCE_PATH
-    REF android-v10.1.0
-    HEAD_REF main
+message(STATUS "Checkout revision ${GIT_REV}")
+vcpkg_execute_required_process(
+    COMMAND ${GIT} checkout ${GIT_REV}
+    WORKING_DIRECTORY ${SOURCE_PATH}
+    LOGNAME checkout
 )
-
-if (VCPKG_HOST_IS_WIN32)
-    # fix git long path issue on windows
-    vcpkg_execute_required_process(
-        COMMAND  ${GIT_EXECUTABLE} config core.longpaths true
-        WORKING_DIRECTORY ${SOURCE_PATH}
-        LOGNAME configure-long-paths-git-${TARGET_TRIPLET}
-    )
-endif ()
 
 vcpkg_execute_required_process(
-    COMMAND  ${GIT_EXECUTABLE} submodule update --init
+    COMMAND ${GIT} submodule update --init
     WORKING_DIRECTORY ${SOURCE_PATH}
     LOGNAME update-submodules-${TARGET_TRIPLET}
 )
 
 vcpkg_execute_required_process(
-    COMMAND  ${GIT_EXECUTABLE} submodule update --init
+    COMMAND ${GIT} submodule update --init
     WORKING_DIRECTORY ${SOURCE_PATH}/vendor/mapbox-base
     LOGNAME update-mapbox-submodules-${TARGET_TRIPLET}
 )
@@ -50,25 +47,29 @@ vcpkg_execute_required_process(
 vcpkg_apply_patches(
     SOURCE_PATH ${SOURCE_PATH}
     PATCHES
-        cmake-changes.patch
-        timer-overflow.patch
+    cmake-changes.patch
+    timer-overflow.patch
+    boost-numeric.patch
+    fix-includes.patch
+    http2.patch
 )
-    
+
+if(VCPKG_TARGET_IS_OSX)
+    set(ADDITIONAL_ARGS -DCMAKE_CXX_FLAGS=-D_HAS_AUTO_PTR_ETC=0)
+endif()
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
-        -DVCPKG_ALLOW_SYSTEM_LIBS=ON
-        -DMLN_WITH_RTTI=ON
-        -DMLN_WITH_COVERAGE=OFF
-        -DMLN_WITH_WERROR=OFF
-        -DMLN_WITH_QT=ON
-        -DMLN_QT_LIBRARY_ONLY=ON
-        -DMLN_QT_STATIC=ON
-        -DMLN_QT_WITH_INTERNAL_SQLITE=ON
-        ${ADDITIONAL_ARGS}
-    MAYBE_UNUSED_VARIABLES
-        VCPKG_ALLOW_SYSTEM_LIBS
+    -DMLN_WITH_RTTI=ON
+    -DMLN_WITH_COVERAGE=OFF
+    -DMLN_WITH_WERROR=OFF
+    -DMLN_WITH_QT=ON
+    -DMLN_QT_LIBRARY_ONLY=ON
+    -DMLN_QT_STATIC=ON
+    -DMLN_QT_WITH_INTERNAL_SQLITE=ON
+    ${ADDITIONAL_ARGS}
 )
 
 vcpkg_install_cmake()
